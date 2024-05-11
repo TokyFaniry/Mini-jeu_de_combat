976f224c9a9b43b3934ffc736d813c4de049be02
Toky <trazafimbelo1@gmail.com>
2024-05-11 10:59:21 +0300

    1st commit

---
+++ Mini-jeu de combat/Guerrier.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_jeu_de_combat
+{
+    internal class Guerrier
+    {
+        public string Nom { get; set; }
+        public int Attaque { get; set; }
+        public int Defense { get; set; }
+        public int Sante { get; set; }
+        public int Mana { get; set; }
+
+        public Guerrier(string nom, int attaque, int defense, int sante)
+        {
+            Nom = nom;
+            Attaque = attaque;
+            Defense = defense;
+            Sante = sante;
+            Mana = 100;
+        }
+        public Guerrier ( string nom, int attaque , int defense)
+        {
+            Nom=nom; 
+            Attaque=attaque; 
+            Defense=defense; 
+            Sante=1000; 
+            Mana=100;
+        }
+
+        public Guerrier() { }
+
+        public void LancerAttaqueSur(Guerrier g2)
+        {
+            Console.WriteLine(this.Nom + " Attaque...");
+            g2.Sante = g2.Sante - this.Attaque;
+            Console.WriteLine(" Attaquant: " + this.Nom + " Sante : " + this.Sante);
+            Console.WriteLine(" Defense: " + g2.Nom + " Sante : " + g2.Sante);
+        }
+
+        public void LancerAttaqueAvecDefenseSur(Guerrier g2)
+        {
+            Console.WriteLine(this.Nom + " Attaque...");
+            if (this.Attaque>g2.Defense)
+            {
+                g2.Sante = g2.Sante-this.Attaque;
+                Console.WriteLine(" Attaque...");
+            }
+            else
+            {
+                this.Sante = this.Sante- (g2.Defense - this.Attaque);
+                Console.WriteLine(" Attaque echouer");
+            }
+            Console.WriteLine(" Attaquant: " + this.Nom + " Sante : " + this.Sante);
+            Console.WriteLine(" Defense: " + g2.Nom + " Sante : " + g2.Sante);
+        } 
+        public void ActiverSoin()
+        {
+            if (this.Mana > 10)
+            {
+                Console.WriteLine(this.Nom + " Soin...");
+                this.Sante = this.Sante + 10;
+                this.Mana = this.Mana - 10;
+            }
+            else
+            {
+                Console.WriteLine("Vous n'avez pas assez de Mana");
+            }
+            Console.WriteLine(this.Nom + " Sante: " + this. Sante);
+        }
+        public void Afficher()
+        {
+            Console.WriteLine("Nom: "+this.Nom
+                +" Attaque: "+this.Attaque+
+                " Defense: "+this.Defense+
+                " Sante: "+this.Sante);
+        }
+
+
+    }      
+}
---
+++ Mini-jeu de combat/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Mini_jeu_de_combat;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Guerrier guerrier1 = new Guerrier("Toky",200,300);
+        Guerrier guerrier2= new Guerrier("Faniry",200,400);
+
+        guerrier1.Afficher();
+        guerrier2.Afficher();
+
+        guerrier1.LancerAttaqueSur(guerrier2);
+        guerrier2.ActiverSoin();
+
+        guerrier1.Afficher();
+        guerrier2.Afficher();
+
+        guerrier2.LancerAttaqueAvecDefenseSur(guerrier1);
+        guerrier2.ActiverSoin();
+
+
+
+
+    }
+}
+
